e603fa2dec7a1a77d50c9c1d02d6a42a3ceeff5f
Adam Strzelecki <ono@java.pl>
2009-07-10 13:42:15 +0200

    Move all utilities back into main module.

--- lib/sinatra-hat/string_permalink.rb
+++
@@ -1,7 +0,0 @@
-# http://gist.github.com/93045
-require 'iconv'
-class String
-  def to_permalink
-    Iconv.iconv('ascii//translit//IGNORE', 'utf-8', self).first.gsub("'", "").gsub(/[^\x00-\x7F]+/, '').gsub(/[^a-zA-Z0-9-]+/, '-').gsub(/--+/, '-').gsub(/^-/, '').gsub(/-$/, '').downcase
-  end
-end
